480bd9cc0db4457cf1f3c3e0da53bfa684cdb79d
Oleksiy Yevtushok <alexey.yevtushok@gmail.com>
2019-03-25 16:59:58 +0200

    partly tested

--- src/components/bombChecker.js
+++ src/components/bombChecker.js
@@ -1,15 +1,15 @@
 const bombChecker = (array, i, j, columns, rows) => {
   let bombCounter = 0;
-  if (i + 1 < columns && i - 1 >= 0 && j + 1 < rows && j - 1 >= 0) {
-    if (array[i][j + 1] === "bomb") bombCounter++;
-    if (array[i][j - 1] === "bomb") bombCounter++;
-    if (array[i + 1][j] === "bomb") bombCounter++;
-    if (array[i - 1][j] === "bomb") bombCounter++;
-    if (array[i + 1][j - 1] === "bomb") bombCounter++;
-    if (array[i + 1][j + 1] === "bomb") bombCounter++;
-    if (array[i - 1][j + 1] === "bomb") bombCounter++;
-    if (array[i - 1][j - 1] === "bomb") bombCounter++;
-  }
+  if (j + 1 < rows && array[i][j + 1] === "bomb") bombCounter++;
+  if (j - 1 >= 0 && array[i][j - 1] === "bomb") bombCounter++;
+  if (i + 1 < columns && array[i + 1][j] === "bomb") bombCounter++;
+  if (i - 1 >= 0 && array[i - 1][j] === "bomb") bombCounter++;
+  if (i + 1 < columns && j - 1 >= 0 && array[i + 1][j - 1] === "bomb")
+    bombCounter++;
+  if (i + 1 < columns && j + 1 < rows && array[i + 1][j + 1] === "bomb")
+    bombCounter++;
+  if (i - 1 >= 0 && j + 1 < 8 && array[i - 1][j + 1] === "bomb") bombCounter++;
+  if (i - 1 >= 0 && j - 1 >= 0 && array[i - 1][j - 1] === "bomb") bombCounter++;
   return bombCounter;
 };
 
